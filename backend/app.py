0c1e0776ceaea4034ab0a0fa6780d40e0ef06ba9
Mustapha Aouimar <mustapha.aouimar@gmail.com>
2022-06-18 04:40:10 +0000

    add some exclusions

--- backend/app.py
+++ backend/app.py
@@ -25,7 +25,8 @@ origins = [
     "http://localhost",
     "http://localhost:3000",
     "*",
-    "https://deep-graph.herokuapp.com"
+    "https://deep-graph.herokuapp.com",
+    "https://deep-graph-back.herokuapp.com"
 ]
 
 if(DEBUG):
@@ -34,8 +35,7 @@ if(DEBUG):
         "127.0.0.1"
     ]
 else:
-    hosts = ["https://deep-graph.herokuapp.com",
-             "*"]
+    hosts = ["deep-graph.herokuapp.com", "deep-graph-back.herokuapp.com"]
 
 
 app.add_middleware(
@@ -67,7 +67,7 @@ async def authenticate_user(form_data: OAuth2PasswordRequestForm, request: Reque
 
     if form_data.client_secret == "*secret*":
         client_host = request.client.host
-        if(client_host in hosts or "*" in hosts):  # I can't have the real IP, because heroku doesn't afford ip:port by app, so i let every host (even if it's not related frontend (free)); for private communication, it will be necessery to have the exact domain for more security
+        if(client_host in hosts):
             if(form_data.password == "*password*" and form_data.username == "*user*"):
                 fuser = os.environ['APP_USER']
                 fpswd = os.environ['APP_PSWD']
